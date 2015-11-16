a1e536e8205e68873889919b0a057b8ae34d07b2
Kasper Peulen <kasperpeulen@gmail.com>
2015-11-16 13:00:28 +0100

    Initial Commit

---
+++ TodoApp3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TodoApp3
+//
+//  Created by Kasper Peulen on 11/16/15.
+//  Copyright © 2015 Kasper Peulen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
